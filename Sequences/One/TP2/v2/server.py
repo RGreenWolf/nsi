55e2257126829dd5b56581d3646f249b2a8c1765
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2024-10-08 14:06:22 +0200

    patch server start

--- Sequences/One/TP2/v2/server.py
+++ Sequences/One/TP2/v2/server.py
@@ -247,10 +247,14 @@ def commande():
         else:
             print("Commande inconnue")
         print("\n")
-        
 
 threadCmd = threading.Thread(target=commande)
-threadCmd.start()
+
+def startServer():
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+
+threadWeb = threading.Thread(target=startServer)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    threadWeb.start()
+    threadCmd.start()
