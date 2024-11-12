4ea209db27c93cd5bee0409a718f1c98036d143c
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2024-11-12 10:57:17 +0100

    ajout de nouveaux fichiers .gitignore, suppression de fichiers obsolètes, et ajout de la configuration Tailwind CSS

---
+++ Chapitres/Chapitre 3/exercices.py
@@ -0,0 +1,60 @@
+tableau = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def aff_tab_1(tableau): 
+    i = 0
+    while i < len(tableau):
+        print(tableau[i])
+        i += 1
+
+print("Exercice 1 :")
+aff_tab_1(tableau)
+print("\n")
+
+def aff_tab_2(tableau): 
+    for i in range(len(tableau)):
+        print(tableau[i])
+
+print("Exercice 2 :")
+aff_tab_2(tableau)
+print("\n")
+
+def aff_tab_3(tableau):
+    for i in tableau:
+        print(i)
+
+print("Exercice 3 :")
+aff_tab_3(tableau)
+print("\n")
+
+def somme_pairs(tableau):
+    somme = 0
+    for i in range(len(tableau)):
+        if tableau[i] % 2 == 0:
+            somme += tableau[i]
+    return somme
+
+print("Exercice 4 :")
+print(somme_pairs(tableau))
+print("\n")
+
+def minimun(tableau):
+    min = tableau[0]
+    for i in range(len(tableau)):
+        if tableau[i] < min:
+            min = tableau[i]
+    return min
+
+print("Exercice 5 :")
+print(minimun(tableau))
+print("\n")
+
+def maximum(tableau):
+    max = tableau[0]
+    for i in range(len(tableau)):
+        if tableau[i] > max:
+            max = tableau[i]
+    return max
+
+print("Exercice 6 :")
+print(maximum(tableau))
+print("\n")
