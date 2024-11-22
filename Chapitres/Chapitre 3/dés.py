b56443037db23c8a79100499331729a6694149b8
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2024-11-22 13:37:31 +0100

    ajout de commentaires explicatifs dans le script de simulation de dés pour clarifier le code

--- Chapitres/Chapitre 3/dés.py
+++ Chapitres/Chapitre 3/dés.py
@@ -1,17 +1,20 @@
 import random
 
-lancers = [0] * 13
-total_lancer = 10000
+# initialisation
+lancers = [0] * 13 # créer une liste de 13 éléments initialisés à 0
+total_lancer = 10000 # nombre de lancers
 
 for _ in range(total_lancer):
+    # somme des dés
     somme = random.randint(1, 6) + random.randint(1, 6)
     lancers[somme] += 1
 
-print(lancers)
+max_lancers = max(lancers[2:]) # nombre de fois que le nombre le plus fréquent a été lancé
+scale = 75 / max_lancers # échelle pour afficher les étoiles
 
-max_lancers = max(lancers[2:])
-scale = 75 / max_lancers
-
+# print le nombre de fois que chaque nombre a été lancé
 for nombre in range(2, 13):
+    # etoiles = nombre d'étoiles correspondant au nombre de fois que le nombre a été lancé multiplié par l'échelle
     etoiles = int(lancers[nombre] * scale)
+    # print imprimer le nombre d'étoiles correspondant au nombre de fois que le nombre a été lancé
     print(f"Nombre {nombre}: {lancers[nombre]} fois, Probabilité: {lancers[nombre] / total_lancer:.4f}  {'*' * etoiles}")
